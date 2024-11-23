f98ee49fd71b7f0c8c04d300ffe9c350f35e14d0
davidebizzocchi <davidebizzocchi19@gmail.com>
2024-11-23 23:20:01 +0100

    deleteView e upgrade

--- django_cat/users/views.py
+++ django_cat/users/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
-from .forms import UserRegistrationForm
-from .models import UserProfile
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from users.forms import UserRegistrationForm, LoginForm  # Aggiungere LoginForm all'import
+from users.models import UserProfile
+
 from icecream import ic
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from django.utils.decorators import method_decorator
 
 class RegisterUserView(UserPassesTestMixin, CreateView):
     form_class = UserRegistrationForm
@@ -33,12 +38,17 @@ class RegisterUserView(UserPassesTestMixin, CreateView):
         # Create UserProfile
         UserProfile.objects.create(user=user)
         
+        # Auto-login dopo la registrazione con backend specificato
+        authenticate(self.request, username=user.username)
+        login(self.request, user, backend='django.contrib.auth.backends.ModelBackend')
+        
         return super().form_valid(form)
 
 class UserLoginView(LoginView):
     template_name = 'users/login.html'
     success_url = reverse_lazy('home')
     redirect_authenticated_user = True
+    form_class = LoginForm  # Cambiato da UserRegistrationForm a LoginForm
 
     def form_valid(self, form):
         username = form.cleaned_data.get('username')
@@ -47,7 +57,7 @@ class UserLoginView(LoginView):
         user = authenticate(self.request, username=username)
         if user is not None:
             login(self.request, user)
-            return redirect(self.get_success_url())
+            return redirect(self.success_url)
         return super().form_invalid(form)
 
 class UserLogoutView(LoginRequiredMixin, LogoutView):
@@ -59,14 +69,38 @@ class UserLogoutView(LoginRequiredMixin, LogoutView):
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
 
-    # def get(self, request, *args, **kwargs):
-    #     # Se l'utente non è autenticato, redirect alla home
-    #     if not request.user.is_authenticated:
-    #         return redirect('home')
-    #     # Altrimenti mostra la pagina di conferma
-    #     return render(request, self.template_name)
+class UserProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'users/profile.html'
+    login_url = reverse_lazy('users:login')
+    
+    def get(self, request, *args, **kwargs):
+        user_profile: UserProfile = request.user.userprofile
+        context = {
+            'username': request.user.username,
+            'cheshire_id': user_profile.cheschire_id
+        }
+        return render(request, self.template_name, context)
 
-    # def post(self, request, *args, **kwargs):
-    #     # Esegue il logout
-    #     logout(request)
-    #     return redirect(self.next_page)
+@method_decorator(require_http_methods(["POST"]), name='dispatch')
+class DeleteUserView(LoginRequiredMixin, TemplateView):
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        logout(request)
+        user.delete()
+        return JsonResponse({"status": "success", "redirect_url": reverse_lazy('home')})
+
+class UserListView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
+    template_name = 'users/list.html'
+    login_url = reverse_lazy('users:login')
+    
+    def test_func(self):
+        return self.request.user.is_staff
+    
+    def handle_no_permission(self):
+        return redirect('home')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['users'] = User.objects.all()
+        ic(context)
+        return context
