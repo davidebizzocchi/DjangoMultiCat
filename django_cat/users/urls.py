e926cb1047b0501938ea7b85e0edc29f9b201735
davidebizzocchi <davidebizzocchi19@gmail.com>
2024-11-23 23:19:42 +0100

    aggiunti url profile, delete, list a user

--- django_cat/users/urls.py
+++ django_cat/users/urls.py
@@ -1,6 +1,5 @@
-
 from django.urls import path
-from .views import RegisterUserView, UserLoginView, UserLogoutView
+from .views import RegisterUserView, UserLoginView, UserLogoutView, UserProfileView, DeleteUserView, UserListView
 
 app_name = 'users'
 
@@ -8,4 +7,7 @@ urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path("delete/", DeleteUserView.as_view(), name="delete"),
+    path('list/', UserListView.as_view(), name='list'),
 ]
