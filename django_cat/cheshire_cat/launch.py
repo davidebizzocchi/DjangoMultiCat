028673cd006a4c78efd6c7a0c5071c9b8c71e48b
davidebizzocchi <davidebizzocchi19@gmail.com>
2024-11-23 17:38:58 +0100

    cheschire_cat launch

--- django_cat/cheshire_cat/launch.py
+++ django_cat/cheshire_cat/launch.py
@@ -1,15 +1,23 @@
 from icecream import ic
-from time import sleep
+import time
 import cheshire_cat_api as ccat
 import requests
+from django.dispatch import receiver
+from app.signals import server_start
+
+from cheshire_cat.client import Cat, CatConfig
+
+cat_client: Cat = None
 
 def main():
     ic("ciao")
-    response, user_id = None
-    while True:
-        sleep(1)
+    global cat_client
 
-        url = "http://localhost:1865/users/"
+    response, user_id = None, None
+    while True:
+        time.sleep(1)
+
+        url = "http://cheshire-cat-core:80/users/"
         response = requests.get(url)
 
         ic(response.status_code)
@@ -26,5 +34,28 @@ def main():
                 break
 
     
+    # if user_id is not None:
+
     if user_id is not None:
-        config = ccat.Config(user_id=user_id)
+        config = CatConfig(user_id=user_id, base_url="cheshire-cat-core", port=80)
+        cat_client = Cat(
+            config=config
+        )
+
+        cat_client.connect_ws()
+        ic("connect")
+
+        while not cat_client.is_ws_connected: 
+        # A better handling is strongly advised to avoid an infinite loop 
+            time.sleep(1)
+
+        ic("CONNESS")
+
+
+@receiver(server_start)
+def start_cheshire_cat(sender=None, **kwargs):
+    if cat_client is not None:
+        cat_client.close()
+        ic("disconnect")
+
+    main()
