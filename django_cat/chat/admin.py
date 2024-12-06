ba507cf060d1c89ae54b4f4c02abfc520523ba46
davidebizzocchi <davidebizzocchi19@gmail.com>
2024-12-06 11:40:14 +0100

    admin messages

--- django_cat/chat/admin.py
+++ django_cat/chat/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from chat.models import Message
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'sender', 'text_preview', 'timestamp')
+    list_filter = ('sender', 'timestamp', 'user')
+    search_fields = ('text', 'user__username')
+    date_hierarchy = 'timestamp'
+    ordering = ('-timestamp',)
+    
+    def text_preview(self, obj):
+        """Restituisce un'anteprima del testo limitata a 50 caratteri"""
+        return obj.text[:50] + '...' if len(obj.text) > 50 else obj.text
+    text_preview.short_description = 'Text Preview'
