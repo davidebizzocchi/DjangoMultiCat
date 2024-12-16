4c932997597bb30b0833b26d31eb2566df6a2ca4
davidebizzocchi <davidebizzocchi19@gmail.com>
2024-12-17 00:52:32 +0100

    startup

--- django_cat/cheshire_cat/client.py
+++ django_cat/cheshire_cat/client.py
@@ -59,8 +59,15 @@ class Cat(CatClient):
             return super().connect_ws()
 
     def startup(self):
-        if not self.is_ws_connected:
-            self.connect_ws()
+        self.connect_ws()
+
+        counter = 0
+        while not self.is_ws_connected:
+            time.sleep(0.2)
+            counter += 1
+
+            if counter == 100:
+                raise TimeoutError("Cannot connect to the websocket")
 
         return self
 
