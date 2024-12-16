ecb63ca7dca9b0869a980b3cfeba86d88cd5ed5a
davidebizzocchi <davidebizzocchi19@gmail.com>
2024-12-17 00:43:09 +0100

    userprofile admin

--- django_cat/users/admin.py
+++ django_cat/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from users.models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('username', 'cheschire_id', 'is_active')
+    search_fields = ('user__username', 'cheschire_id')
