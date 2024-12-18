31d28ae38695d30e7cb242d72c3ddf179df8a55b
davidebizzocchi <davidebizzocchi19@gmail.com>
2024-12-19 00:42:03 +0100

    files metadata upload

--- django_cat/chat/models.py
+++ django_cat/chat/models.py
@@ -9,6 +9,15 @@ class Chat(BaseUserModel):
     chat_id = models.CharField(max_length=255, unique=True, default=uuid.uuid4)
     libraries = models.ManyToManyField(Library, related_name='chats', blank=True)
 
+    @property
+    def files(self):
+        from user_upload.models import File
+
+        """Returns all files associated with this chat with optimized query"""
+        return File.objects.filter(
+            associations__library__chats=self  # Naviga la relazione
+        ).distinct()
+
     def send_message(self, message):
         """Send message to cat"""
         return self.client.send(message, chat_id=self.chat_id)
@@ -20,17 +29,7 @@ class Chat(BaseUserModel):
     def wait_message_content(self):
         """Wait and return last message content for this specific chat"""
         return self.client.wait_message_content(chat_id=self.chat_id)
-
-    def __str__(self):
-        return f"Chat with {self.user.username}, id: {self.chat_id}"
     
-    def delete(self, *args, **kwargs):
-        """Delete chat and all related messages"""
-        
-        result = self.client.delete_chat(self.chat_id)
-
-        return super().delete(*args, **kwargs)
-        
     def wipe(self):
         """Wipe all messages from chat"""
         return self.client.wipe_chat(self.chat_id)
@@ -38,6 +37,33 @@ class Chat(BaseUserModel):
     def get_history(self):
         """Get chat history"""
         return self.client.get_chat_history(self.chat_id)
+
+    def __str__(self):
+        return f"Chat with {self.user.username}, id: {self.chat_id}"
+    
+    def delete(self, *args, **kwargs):
+        """Delete chat and all related messages"""
+
+        files = self.files
+        
+        result = self.client.delete_chat(self.chat_id)
+
+        super().delete(*args, **kwargs)
+
+        for file in files:
+            self.client.update_file_chats(file)
+
+        return result
+    
+    def save(self, *args, **kwargs):
+        """Save chat and create chat on cat"""
+        from user_upload.models import File
+
+        if not self.pk:
+            for file in self.files:
+                self.client.update_file_chats(file)
+        
+        return super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = "chat"
