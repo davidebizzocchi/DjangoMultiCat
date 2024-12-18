5bc3ae953754f917168384fb3d0961c4473cb915
davidebizzocchi <davidebizzocchi19@gmail.com>
2024-12-18 12:57:38 +0100

    admin library

--- django_cat/chat/admin.py
+++ django_cat/chat/admin.py
@@ -4,14 +4,14 @@ from chat.models import Chat, Message
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('user', 'sender', 'text_preview', 'timestamp')
-    list_filter = ('sender', 'timestamp', 'user')
-    search_fields = ('text', 'user__username', "sender", "chat__chat_id")
+    list_display = ('chat', 'sender', 'text_preview', 'timestamp')
+    list_filter = ('sender', 'timestamp', 'chat')
+    search_fields = ('text', 'chat__user__username', 'sender', 'chat__chat_id')
     date_hierarchy = 'timestamp'
     ordering = ('-timestamp',)
     
     def text_preview(self, obj):
-        """Restituisce un'anteprima del testo limitata a 50 caratteri"""
+        """Returns a preview of the text limited to 50 characters"""
         return obj.text[:50] + '...' if len(obj.text) > 50 else obj.text
     text_preview.short_description = 'Text Preview'
 
