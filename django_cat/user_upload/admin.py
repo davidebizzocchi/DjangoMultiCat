42e183a0169c59a4b912ed5feaa8f88a46b9aa87
davidebizzocchi <davidebizzocchi19@gmail.com>
2024-12-18 12:58:02 +0100

    admin file and file assoc

--- django_cat/user_upload/admin.py
+++ django_cat/user_upload/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from user_upload.models import File, FileLibraryAssociation
 
-# Register your models here.
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('title', 'file_id', 'user', 'ingested', 'library')
+    list_filter = ('ingested', 'library', 'user')
+    search_fields = ('title', 'file_id', 'user__username')
+    readonly_fields = ('file_id', 'hash')
+
+@admin.register(FileLibraryAssociation)
+class FileLibraryAssociationAdmin(admin.ModelAdmin):
+    list_display = ('file', 'library')
+    list_filter = ('library',)
+    search_fields = ('file__title', 'library__name')
