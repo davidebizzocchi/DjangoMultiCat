fe9a26a0bd65d46f7b3e04166729f835bb6194e4
davidebizzocchi <davidebizzocchi19@gmail.com>
2024-12-18 12:57:50 +0100

    admin library

--- django_cat/library/admin.py
+++ django_cat/library/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Library
 
-# Register your models here.
+@admin.register(Library)
+class LibraryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'library_id')
+    search_fields = ('name', 'user__username', 'library_id')
+    list_filter = ('user',)
