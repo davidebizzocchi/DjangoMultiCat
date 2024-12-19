86dea904e4c82471557e028a908382d3cd0018e7
davidebizzocchi <davidebizzocchi19@gmail.com>
2024-12-19 01:29:05 +0100

    endpoint api

--- django_cat/cheshire_cat/custom_objects.py
+++ django_cat/cheshire_cat/custom_objects.py
@@ -640,6 +640,105 @@ class CatMemoryApi(MemoryApi):
             _request_auth=_request_auth
         )
 
+    @validate_call
+    def get_points_by_metadata(
+        self,
+        collection_id: StrictStr,
+        metadata: Dict[StrictStr, Any] = {},
+        _request_timeout: Union[
+            None,
+            Annotated[StrictFloat, Field(gt=0)],
+            Tuple[
+                Annotated[StrictFloat, Field(gt=0)],
+                Annotated[StrictFloat, Field(gt=0)]
+            ]
+        ] = None,
+        _request_auth: Optional[Dict[StrictStr, Any]] = None,
+        _content_type: Optional[StrictStr] = None,
+        _headers: Optional[Dict[StrictStr, Any]] = None,
+        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
+    ) -> object:
+        """Get points filtered by metadata criteria
+        
+        :param collection_id: Collection ID (required)
+        :type collection_id: str
+        :param metadata: Metadata filter criteria
+        :type metadata: dict
+        :param _request_timeout: timeout setting for this request
+        :type _request_timeout: int, tuple(int, int), optional
+        :return: Returns the result object
+        """
+
+        _param = self._get_points_by_metadata_serialize(
+            collection_id=collection_id,
+            metadata=metadata,
+            _request_auth=_request_auth,
+            _content_type=_content_type,
+            _headers=_headers,
+            _host_index=_host_index
+        )
+
+        _response_types_map: Dict[str, Optional[str]] = {
+            '200': "object",
+            '400': "object"
+        }
+        
+        response_data = self.api_client.call_api(
+            *_param,
+            _request_timeout=_request_timeout
+        )
+        response_data.read()
+        return self.api_client.response_deserialize(
+            response_data=response_data,
+            response_types_map=_response_types_map,
+        ).data
+
+    def _get_points_by_metadata_serialize(
+        self,
+        collection_id: str,
+        metadata: Dict[str, Any],
+        _request_auth,
+        _content_type,
+        _headers,
+        _host_index,
+    ) -> Tuple:
+        _host = None
+        _collection_formats: Dict[str, str] = {}
+        _path_params: Dict[str, str] = {'collection_id': collection_id}
+        _query_params: List[Tuple[str, str]] = []
+
+        # Add metadata as query parameters
+        if metadata:
+            _query_params.extend([('metadata', json.dumps(metadata))])
+
+        _header_params: Dict[str, Optional[str]] = _headers or {}
+        _form_params: List[Tuple[str, str]] = []
+        _files: Dict[str, str] = {}
+        _body_params = None
+
+        # set the HTTP header `Accept`
+        _header_params['Accept'] = self.api_client.select_header_accept([
+            'application/json'
+        ])
+
+        # authentication setting
+        _auth_settings: List[str] = []
+
+        return self.api_client.param_serialize(
+            method='GET',
+            resource_path='/memory/collections/{collection_id}/points/by_metadata',
+            path_params=_path_params,
+            query_params=_query_params,
+            header_params=_header_params,
+            body=_body_params,
+            post_params=_form_params,
+            files=_files,
+            auth_settings=_auth_settings,
+            collection_formats=_collection_formats,
+            _host=_host,
+            _request_auth=_request_auth
+        )
+
 class CatClient(ccat.CatClient):
     def _connect_api(self):
         protocol = "https" if self._conn_settings.secure_connection else "http"
