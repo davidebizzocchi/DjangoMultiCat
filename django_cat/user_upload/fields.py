6ae01ba6f3dd46871cfbf8e1166669ca143ce6e8
davidebizzocchi <davidebizzocchi19@gmail.com>
2024-12-24 01:27:17 +0100

    new field for pre config

--- django_cat/user_upload/fields.py
+++ django_cat/user_upload/fields.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 from typing import Optional, Self
 from pydantic import BaseModel, field_serializer, model_validator
+from enum import Enum
 
 
 class FileObject(BaseModel):
@@ -47,4 +48,41 @@ class FileObjectDecoder(json.JSONDecoder):
         if isinstance(obj, str):
             return FileObject(path=obj)
         
+        return obj
+
+class IngestionType(str, Enum):
+    NORMAL = "normal"
+    OCR = "ocr"
+
+class PageMode(str, Enum):
+    SINGLE = "single"
+    DOUBLE = "double"
+
+class IngestionConfig(BaseModel):
+    type: IngestionType = IngestionType.NORMAL
+    mode: PageMode = PageMode.SINGLE
+
+    @property
+    def is_ocr(self) -> bool:
+        return self.type == IngestionType.OCR
+
+    @property
+    def is_double_page(self) -> bool:
+        return self.mode == PageMode.DOUBLE
+
+    # Rimuovi get_choices() visto che ora gestiamo le scelte nel form
+
+class IngestionConfigEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, IngestionConfig):
+            return obj.model_dump()
+        return super().default(obj)
+
+class IngestionConfigDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+    
+    def object_hook(self, obj):
+        if isinstance(obj, dict) and "type" in obj and "mode" in obj:
+            return IngestionConfig(**obj)
         return obj
