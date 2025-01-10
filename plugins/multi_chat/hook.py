c231ae7e9336a4495ab4b8a9f1e2b4dc021ddcb9
davidebizzocchi <davidebizzocchi19@gmail.com>
2025-01-10 21:19:33 +0100

    add user_id in declarative, Close #40

--- plugins/multi_chat/hook.py
+++ plugins/multi_chat/hook.py
@@ -15,3 +15,18 @@ def before_cat_recalls_episodic_memories(episodic_recall_config: dict, cat) -> d
     episodic_recall_config["metadata"]["chat_id"] = cat.chat_id
 
     return episodic_recall_config
+
+@hook()
+def before_rabbithole_insert_memory(doc: Document, cat):
+    doc.metadata["user_id"] = cat.user_id
+
+    return doc
+
+@hook()
+def before_cat_recalls_declarative_memories(declarative_recall_config: dict, cat) -> dict:
+    if not declarative_recall_config.get("metadata"):
+        declarative_recall_config["metadata"] = {}
+
+    declarative_recall_config["metadata"]["user_id"] = cat.user_id
+
+    return declarative_recall_config
