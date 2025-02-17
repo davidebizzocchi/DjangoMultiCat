3f937604f1e52791c78f1ca4b423d8feb5533135
davidebizzocchi <davidebizzocchi19@gmail.com>
2025-02-18 00:53:35 +0100

    update ChatCreateForm

--- django_cat/chat/forms.py
+++ django_cat/chat/forms.py
@@ -2,12 +2,15 @@ from django import forms
 from library.models import Library
 
 class ChatCreateForm(forms.Form):
-    libraries = forms.ModelMultipleChoiceField(
-        queryset=None,
-        widget=forms.CheckboxSelectMultiple,
-        required=False
+    libraries = forms.MultipleChoiceField(
+        choices=[],
+        widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
+        required=False,
+        label="Libraries"
     )
 
     def __init__(self, user, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['libraries'].queryset = Library.objects.filter(user=user)
+        self.fields['libraries'].choices =[
+            (library.library_id, library.name) for library in Library.objects.only("library_id", "name").filter(user=user)
+        ]
