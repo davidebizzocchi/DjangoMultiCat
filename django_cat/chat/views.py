485c4efc2323ca3cc66cdb4d43bf19000a3ad549
davidebizzocchi <davidebizzocchi19@gmail.com>
2025-02-18 15:41:19 +0100

    new chat open dialog

--- django_cat/chat/views.py
+++ django_cat/chat/views.py
@@ -9,6 +9,9 @@ from django.urls import reverse_lazy
 from chat.models import Message, Chat
 from chat.forms import ChatCreateForm
 
+from icecream import ic
+
+
 def home(request):
     return render(request, 'chat/home.html')
 
@@ -43,7 +46,6 @@ class ChatStreamView(ChatMixin, LoginRequiredMixin, DetailView):
         return super().dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
-        chat = get_object_or_404(Chat, chat_id=kwargs['chat_id'])
         
         return super().get(request, *args, **kwargs)
     
@@ -56,21 +58,23 @@ class ChatStreamView(ChatMixin, LoginRequiredMixin, DetailView):
         context["form"] = ChatCreateForm(user=self.usr)
         context["create_thread"] = False
         context["create_thread_immediately"] = False
+
         return context
 
 class ChatCreateView(ChatStreamView):
 
     def get_object(self, *args, **kwargs):
         return {
-            "thread_id": "new"
+                "chat_id": "new",
         }
      
     def pre_dispatch_login(self, *args, **kwargs):
-        if (qs := Chat.objects.filter(user=self.usr)).count() > 0:
-            return redirect(
-                "chat:chat",
-                chat_id=qs.first().chat_id
-            )
+        if self.request.GET.get("force", False):
+            if (qs := Chat.objects.filter(user=self.usr)).count() > 0:
+                return redirect(
+                    "chat:chat",
+                    chat_id=qs.first().chat_id
+                )
         return super().pre_dispatch_login(*args, **kwargs)
 
     def get_context_data(self, **kwargs):
@@ -78,6 +82,9 @@ class ChatCreateView(ChatStreamView):
 
         context["create_thread"] = True
         context["create_thread_immediately"] = False
+        context["force_create_thread"] = Chat.objects.filter(user=self.usr).count() == 0
+
+        ic(context)
 
         return context
 
