2ea3f6ecf91621b2f8c180d0e8ba5c9c2f9b50d9
davidebizzocchi <davidebizzocchi19@gmail.com>
2025-02-20 22:34:53 +0100

    chat form

--- django_cat/chat/forms.py
+++ django_cat/chat/forms.py
@@ -1,7 +1,15 @@
 from django import forms
+from agent.models import Agent
 from library.models import Library
 
 class ChatCreateForm(forms.Form):
+    agent = forms.ChoiceField(
+        choices=[],
+        widget=forms.Select(attrs={'class': 'form-select'}),
+        label="Agent",
+        required=True,
+    )
+
     libraries = forms.MultipleChoiceField(
         choices=[],
         widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
@@ -11,6 +19,11 @@ class ChatCreateForm(forms.Form):
 
     def __init__(self, user, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['libraries'].choices =[
+
+        self.fields['agent'].choices = [("default", "Default")] + [
+            (agent.agent_id, agent.name) for agent in Agent.objects.only("agent_id", "name").filter(user=user)
+        ]
+
+        self.fields['libraries'].choices = [
             (library.library_id, library.name) for library in Library.objects.only("library_id", "name").filter(user=user)
         ]
