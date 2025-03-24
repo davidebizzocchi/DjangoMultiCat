ff33f7501c3c5b330daa1562a13c00e35c4c18ab
davidebizzocchi <davidebizzocchi19@gmail.com>
2025-03-24 01:32:18 +0100

    users admin

--- django_cat/users/admin.py
+++ django_cat/users/admin.py
@@ -1,7 +1,24 @@
-from django.contrib import admin
-from users.models import UserProfile
+"""users/admin.py - Module providing admin functions"""
 
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('username', 'cheshire_id', 'is_active')
-    search_fields = ('user__username', 'cheshire_id')
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import UserProfile, User
+
+
+# Inline per UserProfile nel pannello di amministrazione di User
+class UserProfileInline(admin.StackedInline):
+    """inline edit"""
+
+    model = UserProfile
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    """inline edit"""
+
+    inlines = (UserProfileInline,)
+
+
+# Registra il modello UserProfile e CustomUser nel pannello di amministrazione
+admin.site.register(UserProfile)
+admin.site.register(User)
